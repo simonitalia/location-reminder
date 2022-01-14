6e386eb797f378c8e9e48766a0cdb9e1baf55760
Simon Italia <sitalia@magicaltomato.com>
2022-01-14 02:09:22 +0100

    feature: android tests
    - implement reminder list fragment  navigation test
    
    - initial implementation of ui displayed data test

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,23 +1,39 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.RemindersRepositoryInterface
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.data.local.FakeAndroidTestRepository
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepositoryTest
+import com.udacity.project4.util.RemindersTestUtils
+import kotlinx.android.synthetic.main.fragment_reminder_list.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
@@ -25,7 +41,62 @@ import org.mockito.Mockito.verify
 @MediumTest
 class ReminderListFragmentTest {
 
-//    TODO: test the navigation of the fragments.
-//    TODO: test the displayed data on the UI.
+    private var fakeRepository: RemindersRepositoryInterface? = null
+
+    @Before
+    fun initRepository() {
+        fakeRepository = FakeAndroidTestRepository()
+    }
+
+    @After
+    fun cleanupDb() {
+        fakeRepository = null
+    }
+
+    @Test
+    fun clickAddReminderFab_navigateToSaveReminderFragment() {
+
+        // GIVEN - On the Reminder List screen
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val mockNavController = Mockito.mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.requireView(), mockNavController)
+        }
+
+        // WHEN - Click on the "+" (fab) button
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        // THEN - Verify that we navigate to the Save / Add Reminder Screen
+        Mockito.verify(mockNavController).navigate(
+            ReminderListFragmentDirections.actionReminderListFragmentToSaveReminderFragment()
+        )
+    }
+
+    // execute as run blocking to ensure reminder is added to db before performing with ui test
+    @Test
+    fun savedReminder_DisplayedInUi() = runBlockingTest {
+
+        // GIVEN - Add reminderDto to the DB
+        val reminderDto = RemindersTestUtils.createMockReminderDto()
+        fakeRepository?.saveReminder(reminderDto)
+
+        val fetchedReminder = fakeRepository?.getReminder(reminderDto.id) as Result.Success
+//        Log.i("", "${fetchedReminder.data.title}")
+
+        // WHEN - ReminderListFragment launched to display reminder
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+//        scenario.onFragment {
+//            val adapter = it.remindersRecyclerView.adapter
+//            adapter?.getItemViewType(1)
+//        }
+
+        // THEN - Reminder is displayed on the screen with correct information
+        onView(withText("Test Title"))
+            .check(matches(isDisplayed()))
+    }
+
+
+
+
 //    TODO: add testing for the error messages.
 }
