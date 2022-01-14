185f52dc8d96a63a869ff1566344097631354d79
Simon Italia <sitalia@magicaltomato.com>
2022-01-14 20:43:00 +0100

    feature: android tests
    - implement test in reminder list fragment test to check ui text

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,32 +1,18 @@
 package com.udacity.project4.locationreminders.reminderslist
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.core.app.launchActivity
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
 import com.udacity.project4.locationreminders.data.RemindersRepositoryInterface
-import com.udacity.project4.locationreminders.data.dto.ReminderDTO
-import com.udacity.project4.locationreminders.data.dto.Result
 import com.udacity.project4.locationreminders.data.local.FakeAndroidTestRepository
-import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
-import com.udacity.project4.locationreminders.data.local.RemindersLocalRepositoryTest
 import com.udacity.project4.util.RemindersTestUtils
-import kotlinx.android.synthetic.main.fragment_reminder_list.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
@@ -74,28 +60,23 @@ class ReminderListFragmentTest {
 
     // execute as run blocking to ensure reminder is added to db before performing with ui test
     @Test
-    fun savedReminder_DisplayedInUi() = runBlockingTest {
+    fun savedReminder_checkReminderTextIsDisplayedInUi() = runBlockingTest {
 
         // GIVEN - Add reminderDto to the DB
         val reminderDto = RemindersTestUtils.createMockReminderDto()
         fakeRepository?.saveReminder(reminderDto)
 
-        val fetchedReminder = fakeRepository?.getReminder(reminderDto.id) as Result.Success
-//        Log.i("", "${fetchedReminder.data.title}")
-
-        // WHEN - ReminderListFragment launched to display reminder
+        //WHEN - Reminder List is displayed
         launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
-//        scenario.onFragment {
-//            val adapter = it.remindersRecyclerView.adapter
-//            adapter?.getItemViewType(1)
-//        }
 
-        // THEN - Reminder is displayed on the screen with correct information
-        onView(withText("Test Title"))
-            .check(matches(isDisplayed()))
+        // THEN - Reminder Title text displayed is correct
+        onView(withId(R.id.remindersRecyclerView))
+
+            //Check Title, Description, Location text
+            hasDescendant(withText("Test Title"))
+            hasDescendant(withText("Test Description"))
+            hasDescendant(withText("Test Location"))
     }
-
-
 
 
 //    TODO: add testing for the error messages.
