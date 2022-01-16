52f90ad01962aa5176222cb1898113e0d9a99859
Simon Italia <simonitaliaatwork@gmail.com>
2022-01-16 17:24:55 +0100

    feature: android test
    - implement reminders activity test, end to end test

--- app/src/androidTest/java/com/udacity/project4/locationreminders/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/RemindersActivityTest.kt
@@ -1,16 +1,28 @@
 package com.udacity.project4.locationreminders
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.R
 import com.udacity.project4.locationreminders.data.RemindersRepositoryInterface
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.ReminderListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.RemindersTestUtils
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -65,7 +77,62 @@ class RemindersActivityTest :
         }
     }
 
+    // End to End Testing
 
-//    TODO: add End to End testing to the app
+    @Test
+    fun startActivity_checkAddReminderFlow() = runBlocking {
 
+        // 0. Start Activity
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+
+        /*
+            1. Reminder List Screen
+         */
+
+        // Click Add Reminder
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        /*
+            2.Save Reminder Screen
+         */
+
+        // Perform add Title and add Description
+        onView(withId(R.id.reminderTitle)).perform(replaceText("Reminder Title Test"))
+        onView(withId(R.id.reminderDescription)).perform(replaceText("Reminder Description Test"))
+
+        // Verify Reminder Title and Description displayed on screen
+        onView(ViewMatchers.withText("Reminder Title Test")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withText("Reminder Description Test")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        // Click Select Location
+        onView(withId(R.id.selectLocation)).perform(click())
+
+        /*
+            4. Select Location Screen
+         */
+
+        //Perform add marker on the map and Perform save location
+        onView(withId(R.id.map)).perform(longClick())
+
+        //Click on save location button
+        onView(withId(R.id.saveLocationButton)).perform(click())
+
+        /*
+            5. Save Reminder Screen
+         */
+
+        // Click Save Button
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        /*
+            6. Reminder List Screen
+         */
+
+        // Verify Reminder Title and Description displayed on screen
+        onView(ViewMatchers.withText("Reminder Title Test")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withText("Reminder Description Test")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        //Close scenario
+        activityScenario.close()
+    }
 }
