7396d495410d064772f522907003b61365ea8895
Simon Italia <sitalia@magicaltomato.com>
2022-02-15 16:35:26 +0100

    androidTests: RemindersActivityTestRule
    - implement toast is displayed test
    
    - implement EspressoIdlingResource idling resource
    
    - implement dataBindingIdlingResource idling resource

--- app/src/androidTest/java/com/udacity/project4/locationreminders/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/RemindersActivityTest.kt
@@ -4,19 +4,28 @@ import android.app.Application
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.google.android.material.internal.ContextUtils.getActivity
 import com.udacity.project4.R
 import com.udacity.project4.locationreminders.data.RemindersRepositoryInterface
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.ReminderListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.utils.DataBindingIdlingResource
+import com.udacity.project4.utils.EspressoIdlingResource
+import com.udacity.project4.utils.monitorActivity
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.not
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,7 +35,6 @@ import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.get
-
 
 /**
  * END TO END test to black box test the app
@@ -38,10 +46,13 @@ import org.koin.test.get
 class RemindersActivityTest :
     AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
 
+    // An Idling Resource that waits for Data Binding to have no pending bindings
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
     private lateinit var repository: RemindersRepositoryInterface
     private lateinit var appContext: Application
 
-    /**
+    /*
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
      * at this step we will initialize Koin related code to be able to use it in out testing.
      */
@@ -78,62 +89,82 @@ class RemindersActivityTest :
         }
     }
 
+    /*
+     * Idling resources tell Espresso that the app is idle or busy. This is needed when operations
+     * are not scheduled in the main Looper (for example when executed on a different thread).
+     */
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    /*
+     * Unregister Idling Resource so it can be garbage collected and does not leak any memory.
+     */
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
     // End to End Testing
 
     @Test
     fun startActivity_checkAddReminderFlow() = runBlocking {
 
-        // 0. Start Activity
+        // Start Activity Scenario
         val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
 
         /*
-            1. Reminder List Screen
+             1. Reminder List Screen - Click "+" Add Reminder fab
          */
 
-        // Click Add Reminder
         onView(withId(R.id.addReminderFAB)).perform(click())
 
         /*
-            2.Save Reminder Screen
+            2. Save Reminder Screen -
+            * Perform add Title text
+            * Perform add Description text
+            * Verify Reminder Title text is displayed
+            * Verify Description text is displayed
+            * Perform Click Select Location
          */
 
-        // Perform add Title and add Description
         onView(withId(R.id.reminderTitle)).perform(replaceText("Reminder Title Test"))
         onView(withId(R.id.reminderDescription)).perform(replaceText("Reminder Description Test"))
-
-        // Verify Reminder Title and Description displayed on screen
-        onView(ViewMatchers.withText("Reminder Title Test")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        onView(ViewMatchers.withText("Reminder Description Test")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-
-        // Click Select Location
+        onView(withText("Reminder Title Test")).check(matches(isDisplayed()))
+        onView(withText("Reminder Description Test")).check(matches(isDisplayed()))
         onView(withId(R.id.selectLocation)).perform(click())
 
         /*
-            4. Select Location Screen
+            3. Select Location Screen -
+            * Perform add marker on the map
+            * Perform save location
          */
-
-        //Perform add marker on the map and Perform save location
         onView(withId(R.id.map)).perform(longClick())
-
-        //Click on save location button
         onView(withId(R.id.saveLocationButton)).perform(click())
 
         /*
-            5. Save Reminder Screen
+            4. Save Reminder Screen - Perform Click Save Button
          */
-
-        // Click Save Button
         onView(withId(R.id.saveReminder)).perform(click())
 
         /*
-            6. Reminder List Screen
+            5. Reminder List Screen -
+            * Verify "Reminder Saved !" Toast is displayed
+            * Verify Reminder Title text is displayed
+            * Verify Description text is displayed
+            * Perform Logout
          */
+        onView(withText(R.string.reminder_saved))
+            .inRoot(withDecorView(not(getActivity(appContext)?.window?.decorView)))
+            .check(matches(isDisplayed()))
+        onView(withText("Reminder Title Test")).check(matches(isDisplayed()))
+        onView(withText("Reminder Description Test")).check(matches(isDisplayed()))
 
-        // Verify Reminder Title and Description displayed on screen
-        onView(ViewMatchers.withText("Reminder Title Test")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        onView(ViewMatchers.withText("Reminder Description Test")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-
-        //Close scenario
+        // Close scenario
         activityScenario.close()
     }
 }
